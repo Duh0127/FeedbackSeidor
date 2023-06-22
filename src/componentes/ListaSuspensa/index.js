805f55d585ca36f47142430d79c696c2d96758c4
Duh0127 <rodatupmocgamer22@gmail.com>
2023-06-22 06:34:03 -0300

    Estilização ListaSuspensa

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import './ListaSuspensa.css';
 
-const ListaSuspensa = ({ options, value, onChange }) => {
+const ListaSuspensa = ({ options, value, label, onChange }) => {
   return (
-    <select value={value} onChange={onChange}>
-      <option value="">Selecione</option>
-      {options.map((option) => (
-        <option key={option.value} value={option.value}>{option.label}</option>
-      ))}
+    <div className='listaSuspensa'>
+      <label>{label}</label>
+      <select value={value} onChange={onChange}>
+        <option value="">Selecione</option> {/* Opção padrão */}
+        {options.map((option) => ( // Mapeia as opções
+          <option key={option.value} value={option.value}>{option.label}</option> // Retorna as opções com o valor e o label de cada um
+        ))}
     </select>
+    </div>
   );
 };
 
